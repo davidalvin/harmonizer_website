eeb0778a24f3d2f8646af71a64a5be9699599425
David Alvin <justinbesant@gmail.com>
2020-12-21 02:53:08 +0000

    Auto select dev vs. prod

--- config.py
+++ config.py
@@ -3,20 +3,21 @@ from os import path
 class Config(object):
     DEBUG = False
     SECRET_KEY = 'b45ad7e975774f7f988c6aacb177d22'
-    # SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
-
+    SERVER_NAME = '0.0.0.0:5000'
     # Upload File Quality Checks
     ALLOWED_UPLOAD_FILE_EXTENSIONS = ['MID']
     MAX_CONTENT_LENGTH = 10 * 1024  # 10 kB
 
     # IO Paths
     FILE_UPLOAD_PATH = path.join('webapp', 'input')
-    UPLOADED_FILE_NAME = "user-upload.mid"
     OUTPUT_PATH = path.join("webapp", "output")
-    GENERATED_MIDI_FILE_NAME = "generated-song.mid"
+    # UPLOADED_FILE_NAME = "user-upload.mid"
+    # GENERATED_MIDI_FILE_NAME = "generated-song.mid"
 
 class ProductionConfig(Config):
     pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
+    SECRET_KEY = 'development'
+    SERVER_NAME = '127.0.0.1:5000'
