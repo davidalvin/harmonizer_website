a3d6aaeda2eadf0ff49e162b604cc9fce59fe14c
David Alvin <justinbesant@gmail.com>
2020-12-21 02:55:46 +0000

    Added flask-limiter functionality

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,8 +1,21 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from flask_limiter import Limiter
+from flask_limiter.util import get_remote_address
 
 app = Flask(__name__)
-app.config.from_object('config.DevelopmentConfig')
-# db = SQLAlchemy(app)
+
+if app.config["ENV"] == "development":
+    print("\nRUNNING ON DEVELOPMENT...\n")
+    app.config.from_object('config.DevelopmentConfig')
+else:
+    print("\nRUNNING ON PRODUCTION...\n")
+    app.config.from_object('config.ProductionConfig')
+
+# Limit traffic to the website as it is in alpha
+limiter = Limiter(
+    app,
+    key_func= lambda : "1", # 1 means this applies to all users
+    default_limits=["20 per day", "10 per hour", "1 per 2minutes"]
+)
 
 from webapp import routes
