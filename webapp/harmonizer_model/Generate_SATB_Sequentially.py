fdc0821b433e840e57fa9e14faed3fadb7d6e1dd
David Alvin <justinbesant@gmail.com>
2020-12-21 02:56:37 +0000

    Add ability to enter a customer filename

--- webapp/harmonizer_model/Generate_SATB_Sequentially.py
+++ webapp/harmonizer_model/Generate_SATB_Sequentially.py
@@ -9,25 +9,31 @@ from webapp.harmonizer_model.HelperFunctions import load_midi
 from webapp.harmonizer_model.MelodyGenerator import MelodyGenerator, save_score, encode
 
 
-def generate_harmony_from_melody():
-    # Initialize the song
+def generate_harmony_from_melody(song_name=None, temperatures=[1]):
+    """Generate a song from a melody
+    :song_name: Name of the melody to be harmonized
+    :temperature: A list of temperatures for random sampling. 
+        Note the number is divided by 10, so 1 will become 0.1.
+        Must be entered as a list: e.g. [1, 2, 3]
+    """
+
+    # Initialize the song class by loading in the model and mapping files
     new_song = MelodyGenerator(MODEL_PATH, SYMBOL_MAPPING_PATH)
 
-    pathname = os.path.join(app.config["FILE_UPLOAD_PATH"], app.config["UPLOADED_FILE_NAME"])
+    pathname = os.path.join(app.config["FILE_UPLOAD_PATH"], song_name)
     song = load_midi(pathname)
     encoded_song = encode(song).split()
 
-    # Define an array with different temperatures to test
-
-    # TODO Chenage temperature to a user input
-    temperatures = [1] # Note this number is divide by 10, so 1 will become 0.1
+    #temperatures = [1] # Note this number is divide by 10, so 1 will become 0.1
     temperatures = [float(temp) / 10 for temp in temperatures]
 
     # Iterate through each instance and generate a different test song
     for temp in temperatures:
         B_melody, A_melody, T_melody = new_song.harmonize_melody_sequentially(
             encoded_song, temperature=temp, key=SONG_KEY, verbose=0)
-        save_dir = os.path.join(app.config["OUTPUT_PATH"], app.config["GENERATED_MIDI_FILE_NAME"])
+
+        # TODO If there are multiple song names this will overwrite them. To be updated...
+        save_dir = os.path.join(app.config["OUTPUT_PATH"], song_name)
         # Output the score
         save_score(soprano=encoded_song, bass=B_melody,
                    alto=A_melody, tenor=T_melody, file_name=save_dir)
