2f4c30ead07ebf29bcad92bbdfce0ca85daecc9e
David Alvin <justinbesant@gmail.com>
2020-12-31 07:15:24 +0000

    Updated processing route to handle default melody

--- webapp/routes.py
+++ webapp/routes.py
@@ -20,33 +20,43 @@ def home():
 # The route for generating the song
 @app.route("/processing", methods=["POST", "GET"])
 def processing():
-  if (request.method == 'POST') and (session.get("SONG_KEY", None) is not None):
+  if (request.method != 'POST') or (session.get("SONG_KEY", None) is None):
+    return redirect(url_for("home"))
+
+  song_key=session.get("SONG_KEY")
+  output_file = song_key
+
+  # If a default melody is selected, grab the melody
+  if request.form["default-melody"]:
+    input_file = path.join(app.config["DEFAULT_MELODY_DIR"], request.form["default-melody"])
+  # Else, save the file
+  else:
+    # Check validity / security of the file
     valid, message = FileValidator.valid_upload(request)
-    song_key = session.get("SONG_KEY")
-    print("The song_key is: ", song_key)
-    if valid:
-      f = request.files['file']
-      f.save(path.join(app.config["FILE_UPLOAD_PATH"], song_key))
-      flash(message, "success")
-     
-      Generate_SATB_Sequentially.generate_harmony_from_melody(song_name=song_key)
- 
-      flash("Harmonization complete", "success")
-      session["DOWNLOAD_READY"] = 1
-      midi_path = path.join(app.config["OUTPUT_PATH"], session.get("SONG_KEY"))
-      wav_name = session.get("SONG_KEY")[:-3]+"wav"
-      wav_path = path.join(app.config["OUTPUT_PATH"],  wav_name) # Path to output wav
-      sf_path = path.join(app.config["SF_PATH"], 'FluidR3_GM.sf2') # Path to Sound Fount
-
-      # Convert Midi to Wav using fluidsynth, which is installed as a separate dependency
-      process = subprocess.Popen(['fluidsynth', '-F', wav_path, sf_path, midi_path],
-                     stdout=subprocess.PIPE, 
-                     stderr=subprocess.PIPE)
-
-      # TODO: This currently does not display the downloaded file correctly
-    else:
+    if not valid:
       flash(message, "danger")
       return redirect(url_for("home"))
+    
+    input_file = song_key # input file is given a random name, equivalent to the song key
+    f = request.files['file']
+    f.save(path.join(app.config["FILE_UPLOAD_PATH"], input_file))
+
+  print(f'input: {input_file}, ouput: {output_file}')      
+  Generate_SATB_Sequentially.generate_harmony_from_melody(input_file=input_file, output_file=output_file)
+
+  flash("Harmonization complete", "success")
+  session["DOWNLOAD_READY"] = 1
+  midi_path = path.join(app.config["OUTPUT_PATH"], session.get("SONG_KEY"))
+  wav_name = session.get("SONG_KEY")[:-3]+"wav"
+  wav_path = path.join(app.config["OUTPUT_PATH"],  wav_name) # Path to output wav
+  sf_path = path.join(app.config["SF_PATH"], 'FluidR3_GM.sf2') # Path to Sound Fount
+
+  # Convert Midi to Wav using fluidsynth, which is installed as a separate dependency
+  process = subprocess.Popen(['fluidsynth', '-F', wav_path, sf_path, midi_path],
+                  stdout=subprocess.PIPE, 
+                  stderr=subprocess.PIPE)
+
+  # TODO: This currently does not display the downloaded file correctly
 
   # TODO: Should this be in the if statement?
   return render_template('listen.html', wav_name=wav_name)
@@ -68,13 +78,8 @@ def download():
   else:
     return render_template('index.html')
 
-
 @app.route('/cdn/wav/<path:filename>', methods=["GET"])
 def get_wav(filename):
-  return send_from_directory(path.join(app.root_path, "output"), filename=filename)
-
-@app.route('/cdn/wav/<path:filename>', methods=["GET"])
-def get_wav2(filename):
   return send_from_directory(path.join(app.root_path, "output"), filename=filename)
 
 # Site overloaded
@@ -85,10 +90,4 @@ def site_overloaded(e):
 
 @app.route('/test', methods=["POST"])
 def test():
-  if request.method == "POST" and request.form["default_melody"]:
-    melody_name=request.form["default_melody"]
-    song_name=path.join("default", melody_name)
-    Generate_SATB_Sequentially.generate_harmony_from_melody(song_name=song_key)
-  else:
-    print("no melody uploaded")
-  return render_template('test.html')
+  pass
