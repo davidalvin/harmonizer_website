746f491567a9983f9287fdddc981635e98a6eb03
David Alvin <justinbesant@gmail.com>
2021-01-02 18:38:36 +0000

    Commented out SERVER_NAME variables

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from os import path
 class Config(object):
     DEBUG = False
     SECRET_KEY = 'b45ad7e975774f7f988c6aacb177d22'
-    SERVER_NAME = '0.0.0.0:5000'
+    #SERVER_NAME = '0.0.0.0:5000'
     # Upload File Quality Checks
     ALLOWED_UPLOAD_FILE_EXTENSIONS = ['MID']
     MAX_CONTENT_LENGTH = 10 * 1024  # 10 kB
@@ -13,13 +13,13 @@ class Config(object):
     DEFAULT_MELODY_DIR = 'default'
     OUTPUT_PATH = path.join("webapp", "output")
     SF_PATH = path.join("webapp", "sf") # Soundfont path
-    # UPLOADED_FILE_NAME = "user-upload.mid"
-    # GENERATED_MIDI_FILE_NAME = "generated-song.mid"
 
 class ProductionConfig(Config):
+    #DEBUG = True
+    #SERVER_NAME = 'prelude49.com:5000'
     pass
 
 class DevelopmentConfig(Config):
     DEBUG = True
     SECRET_KEY = 'development'
-    SERVER_NAME = '127.0.0.1:5000'
+    #SERVER_NAME = '127.0.0.1:5000'
